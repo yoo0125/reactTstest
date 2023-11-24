85ed444fa1408788c20c2c6336727ad6089716ab
yoo0125 <yooo0125@naver.com>
2023-11-24 19:00:42 +0900

    ㅇㅇ

--- rets/src/component/PrevPage.tsx
+++ rets/src/component/PrevPage.tsx
@@ -22,9 +22,9 @@ const PrevPage: React.FC = () => {
         <div className='content'>
             <div className='center'>
             <h1>블로그 제목</h1>
-            <p>작성자: John Doe</p>
+            <p>작성자: 홍길동</p>
             <p>
-                블로그 내용이 여기에 들어갑니다. 자세한 내용은 계속해서 추가할 수 있습니다.
+                블로그 내용이 여기에 들어감
             </p>
             </div>
         </div>
